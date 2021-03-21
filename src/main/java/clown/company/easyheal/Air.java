5f8a86adad336004ae2a9c392429ccf26eb47780
zeleney30 <michaelzeleney@gmail.com>
2021-03-21 00:07:28 -0400

    Feed & Air Functionality

--- src/main/java/clown/company/easyheal/Air.java
+++ src/main/java/clown/company/easyheal/Air.java
@@ -1,5 +1,11 @@
 package clown.company.easyheal;
 
+import org.bukkit.entity.Player;
+
 public class Air
 {
+    public Air(Player player)
+    {
+        player.setRemainingAir(player.getMaximumAir());
+    }
 }
--- src/main/java/clown/company/easyheal/Feed.java
+++ src/main/java/clown/company/easyheal/Feed.java
@@ -1,5 +1,11 @@
 package clown.company.easyheal;
 
+import org.bukkit.entity.Player;
+
 public class Feed
 {
+    public Feed(Player player)
+    {
+        player.setFoodLevel(20);
+    }
 }
